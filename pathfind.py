5a105bda7bd487d5a932357c179ccf12ebe20d63
finlaywashere <finman292004@protonmail.com>
2024-09-18 18:55:20 -0400

    added buggy version of pathfinding algorithm, needs to be fixed

--- pathfind.py
+++ pathfind.py
@@ -1,7 +1,7 @@
 import time
 import heapq
 
-class Vertex():
+class Vertex:
     def __init__(self, id, children, edges):
         self.id = id
         self.children = children
@@ -9,16 +9,20 @@ class Vertex():
         self.compute_weighting()
 
     def __str__(self):
-        return str(children)
+        return str(self.id) + " - " + str(self.children)
     def __repr__(self):
         return self.__str__()
     def __hash__(self):
         return self.id
-
+    def depth(self):
+        if len(self.children) > 0 and self.children.values().get(0) is not None:
+            return self.children.values().get(0).depth + 1
+        else:
+            return 1
     def compute_weighting(self):
         """
         Computes an internal weighting for the density of the graph under this vertex
-        Edges are in the format (weight, vertex)
+        Edges are in the format (weight, vertex, subvertex)
         """
         max1 = None
         max2 = None
@@ -38,7 +42,7 @@ class Vertex():
         else:
             self.internal_weight = max1[0]
 
-def Level():
+class Level:
     def __init__(self, vertices, translation):
         self.vertices = vertices
         self.translation = translation
@@ -51,14 +55,14 @@ def Level():
         graph = set()
         for vertex in parent_path:
             graph.update(vertex.children)
-        queue = [[self.vertices[start]]]
-        visited = set()
+        counter = 1
+        queue = [(0,0,[self.vertices[start]])]
         dist = {start: 0}
         while queue:
-            path = heapq.heappop(queue)
+            path = heapq.heappop(queue)[2]
             vertex = path[-1]
-            visited.add(vertex)
-            curr_dist = dist[vertex]
+            curr_dist = dist[vertex.id]
+            print(vertex.depth(), end.depth())
             if vertex == end:
                 print("Found", curr_dist)
                 return path
@@ -67,22 +71,49 @@ def Level():
                 # Compute weighting that accoounts for graph density
                 edge_weighting = edge[0] + vertex.internal_weight + connected_vertex.internal_weight
                 new_dist = curr_dist + edge[0]
-                if dist.get(connected_vertex, 1000000) > new_dist:
-                    dist[connected_vertex] = new_dist
+                if dist.get(connected_vertex.id, 1000000) > new_dist:
+                    dist[connected_vertex.id] = new_dist
                     new_path = list(path)
                     new_path.append(connected_vertex)
-                    heapq.heappush(queue, new_path)
+                    heapq.heappush(queue, (new_dist, counter, new_path))
+                    counter += 1
         print("Not found")
         return None
 
-def group_vertices(level: Level):
+def group_vertices(level: Level, max_in_group):
     vertices = set(level.vertices.values())
     new_vertices = {}
     translation = {}
     id = 0
+    groups = {}
     while len(vertices) > 0:
         vertex = vertices.pop()
-        
+        children = {vertex.id: vertex}
+        translation[vertex.id] = id
+        for edge in vertex.edges:
+            if len(children.keys()) >= max_in_group:
+                break
+            if level.vertices[edge[1]] in vertices:
+                children[edge[1]] = level.vertices[edge[1]]
+                vertices.remove(level.vertices[edge[1]])
+                translation[edge[1]] = id
+        groups[id] = children
+        id += 1
+    for gid,children in groups.items():
+        edges = {}
+        for child in children.values():
+            for edge in child.edges:
+                new_end = translation[edge[1]]
+                if edges.get(new_end, (1000000,None,None))[0] > edge[0]:
+                    edges[new_end] = (edge[0],new_end,child.id)
+        edges_set = set()
+        for edge in edges.values():
+            edges_set.add(edge)
+        vert = Vertex(gid, children, edges_set)
+        new_vertices[gid] = vert
+    return Level(new_vertices, translation)
+
+MAX_IN_GROUP = 50
 
 pre_start = time.time()
 input = open("graph.txt", "r")
@@ -104,20 +135,55 @@ for line in lines:
             spl = c.split(".")
             c = int(spl[0])
             weight = int(spl[1])
-            c_list += [(weight, c)]
+            c_list += [(weight, c, 0)]
             o_list = l1_cons.get(c, [])
-            o_list += [(weight, id)]
+            o_list += [(weight, id, 0)]
             l1_cons[c] = o_list
         l1_cons[id] = c_list
 
 l1_vertices = {}
 for key, value in l1_cons.items():
-    l1_vertices[key] = Vertex(key, None, edges)
+    l1_vertices[key] = Vertex(key, None, value)
 level1 = Level(l1_vertices, {})
-
 levels = [level1]
 while True:
     if len(levels) > 1 and len(levels[-1].vertices.keys()) == len(levels[-2].vertices.keys()):
         print("CONVERGED AT", len(levels), "WITH", len(levels[-1].vertices.keys()), "VERTICES")
-    levels.append(group_vertices(levels[-1])
+        break
+    levels.append(group_vertices(levels[-1], MAX_IN_GROUP))
     print("iteration", len(levels), "-", len(levels[-1].vertices.keys()))
+pre_end = time.time()
+print("Converged in", (pre_end-pre_start), "seconds")
+
+START = 100
+END = 200
+start = time.time()
+
+lowest_common = -1
+
+start_group = START
+end_group = END
+level_maps = {}
+for i in range(1, len(levels)):
+    level = levels[i]
+    level_maps[i] = (start_group, end_group)
+    start_group = level.translation[start_group]
+    end_group = level.translation[end_group]
+    if start_group == end_group:
+        lowest_common = i
+        break
+if lowest_common == -1:
+    print("Disconnected")
+    exit()
+path = [levels[lowest_common+1].vertices[start_group]]
+for i in range(lowest_common, -1, -1):
+    print("level", i)
+    level = levels[i]
+    start_group = level_maps[i][0]
+    end_group = level_maps[i][1]
+    path = level.internal_pathfind(start_group, end_group, path)
+    print("Path length:", len(path))
+
+end = time.time()
+print("Search took", (end-start), "seconds")
+
